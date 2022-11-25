97e2add1f9e2110160cd0739f46d03d5e95e73be
toppaste <stefano.toppan@electrolux.com>
2022-11-25 14:38:34 +0100

    Include model in post view

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -8,5 +9,8 @@ class BlogPost(models.Model):
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
-    	return self.title
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', args=[str(self.id)])
 
